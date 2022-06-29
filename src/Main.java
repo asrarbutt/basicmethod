1e5a4dd33ffe0998ccb1c5edac6b2507afc63c3a
asrarbutt <asrarbutt1986@gmail.com>
2022-06-29 05:40:17 -0700

    add addieren

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("method");
+
+
+        int sum=addieren(3,5);
+        System.out.println(sum);
     }
 
-        public static int duplicate(int value){
+        public static int addieren(int x, int y){
 
-            return value+3;
+            return x+y;
         }
 
 
