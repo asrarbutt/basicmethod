e064c7caeb22f71df3cdf6086b01b55a4333ce37
asrarbutt <asrarbutt1986@gmail.com>
2022-06-29 05:24:54 -0700

    add psvm

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println("method");
     }
+
+        public static int duplicate(int value){
+
+            return value+3;
+        }
+
+
+
 }
