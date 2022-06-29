5e7cdac53f410e8570857877c33497f92ddef51c
asrarbutt <asrarbutt1986@gmail.com>
2022-06-29 06:26:20 -0700

    change method name

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
 
-        int sum=addieren(3,5);
+        int x, y;
+
+        int sum = add(3, 5);
         System.out.println(sum);
+
+        System.out.println(compare(70, 500));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("====Gib Bitte Zwei Zahlen ein!");
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        System.out.println("mit Scanner : " + add(x, y));
+
+        System.out.println("==================================");
+
+        System.out.println(compare(x, y));
 
     }
 
-        public static int addieren(int x, int y){
+    public static int add(int x, int y) {
 
-            return x+y;
+        return x + y;
+    }
+
+    public static boolean compare(int i, int p) {
+
+        if (i > 100 || p > 100) {
+            return true;
+        } else {
+            return false;
+
         }
 
+    }
 
 
 }
